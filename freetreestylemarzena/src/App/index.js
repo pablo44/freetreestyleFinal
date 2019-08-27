eb9b872d50b5117cd6225f3e5313a48284adce1c
Pablo44 <pawelrz@wp.pl>
2019-08-28 01:29:29 +0200

    src/App folder structure

---
+++ freetreestylemarzena/src/App/index.js
@@ -0,0 +1,11 @@
+import React ,{Component} from 'react';
+import "../avoidFOUC"
+import reactDom from 'react-dom'
+import render from './template'
+import './style.scss'
+
+
+class App extends Component {}
+
+
+export default App;
