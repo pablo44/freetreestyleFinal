6378fdebd065af12b94b8deb299bd33034b43e9c
Pablo44 <pawelrz@wp.pl>
2019-08-29 11:24:38 +0200

    StartPage class building

--- freetreestylemarzena/src/StartPage/index.js
+++ freetreestylemarzena/src/StartPage/index.js
@@ -2,6 +2,19 @@ import React, {Component} from 'react';
 import "./startpage.scss";
 import { Link } from "react-router-dom";
 
+class StartPage extends Component {
+    constructor(){
+        super()
+        this.state= {}
+    }
+
+    render (){
+        return(
+            
+        )
+    }
+}
+
 
 
 
