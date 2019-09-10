371fce133b9a9f3edab2592707914fe0155e745c
Pablo44 <pawelrz@wp.pl>
2019-09-10 15:54:09 +0200

    Font awesome with footer icons

--- freetreestylemarzena/src/Footer/index.js
+++ freetreestylemarzena/src/Footer/index.js
@@ -1,18 +1,29 @@
 import React, { Component } from "react";
 import "./style.scss";
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 
 class Footer extends Component {
-    render() {
-        return (
-<footer className="container-fluid p-3 text-center">
+  render() {
+
+    const element2 = <FontAwesomeIcon icon={faFacebook} />
+    const element3 = <FontAwesomeIcon icon={faLinkedin} />
+    const element4 = <FontAwesomeIcon icon={faInstagram} />
+    return (
+      <footer className="container-fluid p-3 text-center">
         <section className="social-icons">
-          <i className="fab fa-facebook-square" />
-          <i className="fab fa-twitter" />
-          <i className="fab fa-instagram" />
+
+          
+          <i>{element2}</i>
+          <i>{element3}</i>
+          <i>{element4}</i>
         </section>
         <div className="footer-copy text-center" />
       </footer>
-        );
-    }
+    );
+  }
 }
 export default Footer;
